161c42a929806c69b934b1ca50233c49c56d7ea4
Trent <trent@Trents-MacBook-Pro.local>
2020-12-14 20:27:16 +1000

    before adding tabbed bar

---
+++ TabbedApp/ShoppingController.swift
@@ -0,0 +1,19 @@
+//
+//  ShoppingController.swift
+//  TabbedApp
+//
+//  Created by Trent on 14/12/20.
+//
+
+import UIKit
+
+class ShoppingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ TabbedApp/TaskController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabbedApp
+//
+//  Created by Trent on 14/12/20.
+//
+
+import UIKit
+
+class TaskController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
